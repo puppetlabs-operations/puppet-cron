052d1779535ebfdefaa509644d9a539f00ecb11e
Gene Liverman <gene.liverman+14713432@puppet.com>
2021-04-29 09:38:56 -0400

    Generate sha256 checksums of built files

--- build.sh
+++ build.sh
@@ -23,6 +23,8 @@ for target in $targets; do
   echo "----> Building project for: $target"
   GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o $output
 
+  sha256sum $output |cut -d ' ' -f1 > $output-sha256sum.txt
+
   zip -j $output.zip $output > /dev/null 2>&1
   tar -czvf $output.tgz $output > /dev/null 2>&1
 done
